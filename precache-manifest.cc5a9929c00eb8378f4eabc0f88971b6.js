9269f1cbeba0a3e22bc5b9f070de013a8c4a0a57
Aze <azrielle101@gmail.com>
2020-08-18 01:12:36 +0800

    Updates

---
+++ precache-manifest.cc5a9929c00eb8378f4eabc0f88971b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e7528c05722e551e019cb5c0bca496b",
+    "url": "/quizzap/index.html"
+  },
+  {
+    "revision": "b3a46dcb268083a5c592",
+    "url": "/quizzap/static/css/main.5a1939fc.chunk.css"
+  },
+  {
+    "revision": "5e5eb2552abb6810bab0",
+    "url": "/quizzap/static/js/2.a5682900.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quizzap/static/js/2.a5682900.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3a46dcb268083a5c592",
+    "url": "/quizzap/static/js/main.b2426f89.chunk.js"
+  },
+  {
+    "revision": "032edd12c327a23557c2",
+    "url": "/quizzap/static/js/runtime-main.47b45ad8.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/quizzap/precache-manifest.cc5a9929c00eb8378f4eabc0f88971b6.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/quizzap/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
