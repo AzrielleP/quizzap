9d9d5f935316277bf781c3cfbcb95bd743ead189
Aze <azrielle101@gmail.com>
2020-10-09 23:22:44 +0800

    Updated the options and text

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -12,19 +12,19 @@ function Intro(props) {
         <h1>
           Welcome to <span className="title">Quizzap</span>
         </h1>
-        <p className="subtext">Answer all questions!</p>
+        <p className="subtext">Answer all questions within the time limit!</p>
 
         <div className="selector">
           <p>Category</p>
           <select className="category-selector">
-            <option value="logic-circuits">Logic Circuits</option>
+            <option value="general-knowledge">General Knowledge</option>
           </select>
         </div>
 
         <div className="selector">
           <p>Difficulty</p>
           <select className="difficulty-selector">
-            <option value="easy">Medium</option>
+            <option value="easy">Easy</option>
           </select>
         </div>
 
