706f8a4aa795ba5fac8d2ea9a5b32dc89f016f4f
Aze <azrielle101@gmail.com>
2020-10-09 22:10:45 +0800

    Updated the code to use the question state for calculations and destructured the props

--- src/components/Result.js
+++ src/components/Result.js
@@ -4,17 +4,18 @@ This component calculates and shows the user's scores.
 =====================================*/
 
 import React from 'react';
-import repo from './questions.js';
+
 
 function Result(props) {
+  const {handleIntro, handleStartQuiz, question} = props;
   const passingPercent = 0.8;
 
   function calcScore() {
     let score = 0;
-    for (let i = 0; i < repo.length; i++) {
+    for (let i = 0; i < question.content.length; i++) {
       // Check if localStorage for index i has a value.
       if (localStorage.getItem(i.toString()) !== 'null') {
-        if (repo[i][localStorage.getItem(i)].correct) {
+        if (localStorage.getItem(i) === question.content[i].correct_answer) {
           score++;
         }
       }
@@ -26,18 +27,18 @@ function Result(props) {
     <div className="resultsContainer">
       <p>Your score is</p>
       <p className="score">
-        {calcScore()}/{repo.length}
+        {calcScore()}/{question.content.length}
       </p>
       <p className="result">
-        {calcScore() >= passingPercent * repo.length
+        {calcScore() >= passingPercent * question.content.length
           ? 'You Passed!'
           : 'You Failed.'}
       </p>
       <div className="resultButtonContainer">
-        <button className="tryAgain" onClick={props.handleStartQuiz}>
+        <button className="tryAgain" onClick={handleStartQuiz}>
           Try Again
         </button>
-        <button className="backToMenu" onClick={props.handleIntro}>
+        <button className="backToMenu" onClick={handleIntro}>
           Back to Menu
         </button>
       </div>
