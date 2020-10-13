fd6ae0f4bf5e8b3451d06b5e9d2d32d1933af900
Aze <azrielle101@gmail.com>
2020-10-13 18:00:26 +0800

    Created a separate file for Result's styling, added states inside Result.js that will determine if the user passed and add conditional styling / rendering

--- src/components/Result.js
+++ src/components/Result.js
@@ -3,44 +3,49 @@ Result is a child component of App.
 This component calculates and shows the user's scores.
 =====================================*/
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ResultPassed from './SVG/Result-Passed';
 import ResultFailed from './SVG/Result-Failed';
+import './css/Result.css';
 
 function Result(props) {
   const { handleIntro, handleStartQuiz, question } = props;
-  const passingPercent = 0.8;
+  const [score, setScore] = useState(0);
+  const [isFail, setFail] = useState(true);
 
-  function calcScore() {
-    let score = 0;
-    for (let i = 0; i < question.content.length; i++) {
-      // Check if localStorage for index i has a value.
-      if (localStorage.getItem(i.toString()) !== 'null') {
-        if (localStorage.getItem(i) === question.content[i].correct_answer) {
-          score++;
+  useEffect(() => {
+    const passingPercent = 0.7;
+
+    function calcScore() {
+      let score = 0;
+      for (let i = 0; i < question.content.length; i++) {
+        // Check if localStorage for index i has a value.
+        if (localStorage.getItem(i.toString()) !== 'null') {
+          if (localStorage.getItem(i) === question.content[i].correct_answer) {
+            score++;
+          }
         }
       }
+      return score;
     }
-    return score;
-  }
+
+    setScore(calcScore());
+    if (score >= passingPercent * question.content.length) {
+      setFail(false);
+    }
+  }, [question, score]);
 
   return (
-    <div className = "resultsContainer">
+    <div className="resultsContainer">
       <div className="scoreContainer">
         <p>Your score is</p>
         <p className="score">
-          {calcScore()}/{question.content.length}
+          {score}/{question.content.length}
         </p>
-        <div className = "svgContainer">
-        {calcScore() >= passingPercent * question.content.length
-            ? <ResultPassed />
-            : <ResultFailed />}
+        <div className="svgContainer">
+          {isFail ? <ResultFailed /> : <ResultPassed />}
         </div>
-        <p className="result">
-          {calcScore() >= passingPercent * question.content.length
-            ? 'You Passed!'
-            : 'You Failed.'}
-        </p>
+        <p className="result">{isFail ? 'You Failed.' : 'You Passed!'}</p>
         <div className="resultButtonContainer">
           <button className="tryAgain" onClick={handleStartQuiz}>
             Try Again
@@ -50,28 +55,33 @@ function Result(props) {
           </button>
         </div>
       </div>
-      <div className = "reviewContainer">
-          <p>Your Quiz Review</p>
-          <ul className = "quizReview">
-            {question.content.map((item, index) => {
-                return <li className = "itemContainer" key = {index}>
-                  <div className = "checkContainer">
-                    <i className ="fas fa-check"></i>
-                  </div>
-                  <div className = "item">
-                    <p className = "itemQuestion">{item.question}</p>
-                    <p className = "userAnswer">
-                      Your Answer: 
-                      <span className = "answer">
-                      {localStorage.getItem(index) !== "null" ? localStorage.getItem(index) : "You don't have an answer." }
-                      </span>
-                    </p>
-                  </div>
-                </li>
-
-              })}
-          </ul>
-           
+      <div className="reviewContainer">
+        <p className="title">Your Quiz Review</p>
+        <ul className="quizReview">
+          {question.content.map((item, index) => {
+            return (
+              <li className="itemContainer" key={index}>
+                <div className="checkContainer">
+                  {localStorage.getItem(index) !== item.correct_answer ? (
+                    <i className="far fa-times-circle"></i>
+                  ) : (
+                    <i className="far fa-check-circle"></i>
+                  )}
+                </div>
+                <div className="item">
+                  <p className="itemQuestion">
+                    {item.question}
+                    <span className="answer">
+                      {localStorage.getItem(index) !== 'null'
+                        ? localStorage.getItem(index)
+                        : "You don't have an answer."}
+                    </span>
+                  </p>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
       </div>
     </div>
   );
