9ca0f233d751e051f0a30dc8a587b449dc801fd7
Aze <azrielle101@gmail.com>
2020-10-13 10:15:52 +0800

    Removed the inline SVG and created separate components for each. Removed question.js file. Changed from 20 questions to just 10 questions. Added a review quiz portion in Result.js that shows the user's right and wrong answers. Editted styling.

--- src/components/Result.js
+++ src/components/Result.js
@@ -4,6 +4,8 @@ This component calculates and shows the user's scores.
 =====================================*/
 
 import React from 'react';
+import ResultPassed from './SVG/Result-Passed';
+import ResultFailed from './SVG/Result-Failed';
 
 function Result(props) {
   const { handleIntro, handleStartQuiz, question } = props;
@@ -23,23 +25,53 @@ function Result(props) {
   }
 
   return (
-    <div className="resultsContainer">
-      <p>Your score is</p>
-      <p className="score">
-        {calcScore()}/{question.content.length}
-      </p>
-      <p className="result">
+    <div className = "resultsContainer">
+      <div className="scoreContainer">
+        <p>Your score is</p>
+        <p className="score">
+          {calcScore()}/{question.content.length}
+        </p>
+        <div className = "svgContainer">
         {calcScore() >= passingPercent * question.content.length
-          ? 'You Passed!'
-          : 'You Failed.'}
-      </p>
-      <div className="resultButtonContainer">
-        <button className="tryAgain" onClick={handleStartQuiz}>
-          Try Again
-        </button>
-        <button className="backToMenu" onClick={handleIntro}>
-          Back to Menu
-        </button>
+            ? <ResultPassed />
+            : <ResultFailed />}
+        </div>
+        <p className="result">
+          {calcScore() >= passingPercent * question.content.length
+            ? 'You Passed!'
+            : 'You Failed.'}
+        </p>
+        <div className="resultButtonContainer">
+          <button className="tryAgain" onClick={handleStartQuiz}>
+            Try Again
+          </button>
+          <button className="backToMenu" onClick={handleIntro}>
+            Back to Menu
+          </button>
+        </div>
+      </div>
+      <div className = "reviewContainer">
+          <p>Your Quiz Review</p>
+          <ul className = "quizReview">
+            {question.content.map((item, index) => {
+                return <li className = "itemContainer" key = {index}>
+                  <div className = "checkContainer">
+                    <i className ="fas fa-check"></i>
+                  </div>
+                  <div className = "item">
+                    <p className = "itemQuestion">{item.question}</p>
+                    <p className = "userAnswer">
+                      Your Answer: 
+                      <span className = "answer">
+                      {localStorage.getItem(index) !== "null" ? localStorage.getItem(index) : "You don't have an answer." }
+                      </span>
+                    </p>
+                  </div>
+                </li>
+
+              })}
+          </ul>
+           
       </div>
     </div>
   );
