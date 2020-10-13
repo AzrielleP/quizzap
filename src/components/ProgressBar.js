c9a76668de4f0af4fd5e0b9e2d0d98b9317087c2
Aze <azrielle101@gmail.com>
2020-10-13 18:07:41 +0800

    Added helpful comments

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -1,3 +1,8 @@
+/*=====================================
+ProgressBar is a child component of Quiz. 
+It shows how many answers the user has answered.
+=====================================*/
+
 import React, { useState, useEffect } from 'react';
 import './css/ProgressBar.css';
 
@@ -6,9 +11,14 @@ function ProgressBar() {
   const [answeredItems, setAnsweredItems] = useState(0);
   const storageContents = Object.entries(localStorage);
 
+  /* storageContents returns an array of arrays, which looks like this:
+  [ [key, value], [key,value], ...]
+  */
+
   useEffect(() => {
     let count = 0;
     for (let element of storageContents) {
+      // Check if the value of the key is not equal to null.
       if (element[1] !== 'null') {
         count++;
       }
