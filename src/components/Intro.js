3019a1ee32afd20d9e27d7ca547d0e84f3781a6b
Aze <azrielle101@gmail.com>
2020-10-13 18:13:59 +0800

    Formatted the file

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -8,12 +8,18 @@ import IntroSvg from './SVG/IntroSvg';
 import './css/Intro.css';
 
 function Intro(props) {
-  const {handleStartQuiz, handleQuizDifficulty, handleQuizCategory, difficulty, category} = props;
+  const {
+    handleStartQuiz,
+    handleQuizDifficulty,
+    handleQuizCategory,
+    difficulty,
+    category,
+  } = props;
 
   const handleSubmit = (event) => {
     event.preventDefault();
     handleStartQuiz();
-  }
+  };
 
   return (
     <div className="intro-container">
@@ -21,35 +27,37 @@ function Intro(props) {
         <h1>
           Welcome to <span className="title">Quizzap</span>
         </h1>
-        <p className="subtext">Answer all trivia questions within the time limit!</p>
+        <p className="subtext">
+          Answer all trivia questions within the time limit!
+        </p>
 
-        <form onSubmit = {handleSubmit} >
+        <form onSubmit={handleSubmit}>
           <div className="selector">
             <p>Category</p>
-            <select 
+            <select
               className="category-selector"
-              value = {category}
-              onChange = {handleQuizCategory}
-              name = "category"
+              value={category}
+              onChange={handleQuizCategory}
+              name="category"
             >
               <option value="9">General Knowledge</option>
-              <option value = "11">Film</option>
-              <option value = "12">Music</option>
-              <option value = "14">Television</option>
-              <option value = "15">Video Games</option>
-              <option value = "17">Science & Nature</option>
-              <option value = "18">Computers</option>
-              <option value = "21">Sports</option>
+              <option value="11">Film</option>
+              <option value="12">Music</option>
+              <option value="14">Television</option>
+              <option value="15">Video Games</option>
+              <option value="17">Science & Nature</option>
+              <option value="18">Computers</option>
+              <option value="21">Sports</option>
             </select>
           </div>
 
           <div className="selector">
             <p>Difficulty</p>
-            <select 
+            <select
               className="difficulty-selector"
-              value = {difficulty}
-              onChange = {handleQuizDifficulty}
-              name = "difficulty"
+              value={difficulty}
+              onChange={handleQuizDifficulty}
+              name="difficulty"
             >
               <option value="easy">Easy</option>
               <option value="medium">Medium</option>
@@ -57,9 +65,7 @@ function Intro(props) {
             </select>
           </div>
 
-          <button className="startQuiz">
-            Take Quiz
-          </button>
+          <button className="startQuiz">Take Quiz</button>
         </form>
       </div>
       <div className="side-svg-container">
